84b2a302fcea9dfcff99b4148fe98f17ebdf4c34
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2022-09-30 19:45:43 +0200

    Add version.c
    Note: weird, but using touch.exe on version.c before compiling causes arm-gdb.exe crash

---
+++ softphone/inc/version.h
@@ -0,0 +1,7 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+extern const char* APP_VERSION;
+extern const char* BUILD_TIMESTAMP;
+
+#endif
---
+++ softphone/src/version.c
@@ -0,0 +1,5 @@
+#include "version.h"
+
+const char* APP_VERSION = "1.0.0";
+
+const char* BUILD_TIMESTAMP = __DATE__", "__TIME__;
