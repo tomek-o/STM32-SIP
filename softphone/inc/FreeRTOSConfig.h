7299d8d1eaabcc4efeb5b059ef4fd6b29c54439d
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2022-09-30 20:17:11 +0200

    Make configASSERT more visible in log

--- softphone/inc/FreeRTOSConfig.h
+++ softphone/inc/FreeRTOSConfig.h
@@ -121,7 +121,7 @@ See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
 /* Normal assert() semantics without relying on the provision of an assert.h
 header file. */
 /* USER CODE BEGIN 1 */
-#define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); printf("ASSERT: "#x", %s/%03d\n", __FILE__, __LINE__); for( ;; ); }
+#define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); printf("\n\nASSERT: "#x", %s/%03d\n\n", __FILE__, __LINE__); for( ;; ); }
 /* USER CODE END 1 */
 
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
