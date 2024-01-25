589bb2d28b5ec2af0bd477873f869df084089e65
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2024-01-25 18:50:07 +0100

    Add "mem_stat_dump" shell command

--- softphone/src/mem_stat.c
+++ softphone/src/mem_stat.c
@@ -1,4 +1,5 @@
 #include "mem_stat.h"
+#include "shell.h"
 #include "FreeRTOS.h"
 #include <stdio.h>
 
@@ -14,4 +15,15 @@ void mem_stat_dump(void)
            stats.xNumberOfSuccessfulAllocations, stats.xNumberOfSuccessfulFrees,
            stats.xNumberOfSuccessfulAllocations - stats.xNumberOfSuccessfulFrees
            );
+}
+
+
+static enum shell_error sh_mem_stat_dump(int argc, char ** argv) {
+    mem_stat_dump();
+    return SHELL_ERR_NONE;
+}
+
+static __attribute__((constructor)) void registerCmd(void)
+{
+	shell_add("mem_stat_dump", (void*)sh_mem_stat_dump, "Show heap usage stats");
 }
