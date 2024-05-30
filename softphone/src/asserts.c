82874936ae3c8e49a8cd7f0501971390eccb3ca3
unknown <tomasz.o.ostrowski@gmail.com>
2024-05-30 23:23:47 +0200

    Add custom assert function

--- softphone/src/asserts.c
+++ softphone/src/asserts.c
@@ -6,8 +6,21 @@
   */
 void Error_Handler(void)
 {
-  printf("\n\nError_Handler!\n\n");
-  while (1)
-  {
-  }
+    printf("\n\nError_Handler!\n\n");
+    __asm volatile ("bkpt 0");
+    while (1)
+    {
+    }
+}
+
+void __assert_func(const char *_file, int _line, const char *_func, const char *_expr )
+{
+    printf("ASSERT: file %s, line %d, func %s: %s\n", _file , _line, _func?_func:"NULL", _expr);
+    volatile int loop = 1;
+    __asm volatile ("bkpt 0");
+    do
+    {
+        ;
+    }
+    while( loop );
 }
